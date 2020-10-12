060ba559b102e080bff758de01955679e9fdad2c
Nick Rose <nickfbp@gmail.com>
2020-10-12 16:08:05 -0600

    Classes part 2

--- src/playground/es6-classes1.js
+++ src/playground/es6-classes1.js
@@ -9,12 +9,45 @@ class Person {
     getDescription() {
         return `${this.name} is ${this.age} years old.`
     }
+
 }
 
-const me = new Person('Nick Rose', 33);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`
+        }
+        
+        return description;
+    }
+}
 
-const other = new Person();
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += ` I am visiting from ${this.homeLocation}.`
+        }
+        return greeting;
+    }
+}
+
+
+const me = new Traveler('Nick Rose', 33, 'ABQ');
+console.log(me.getGreeting());
+
+const other = new Traveler();
 console.log(other.getGreeting());
-console.log(other.getDescription());
