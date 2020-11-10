96e735093ffb1ee6f196df0b27e59cf6fefb528a
Nick Rose <nickfbp@gmail.com>
2020-11-10 16:47:28 -0700

    One Component per File

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 81));
-
-
-console.log(isAdult(33));
-console.log(canDrink(19));
-console.log(isSenior(67));
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
