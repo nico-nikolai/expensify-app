64e9461273847df045e1672ee702508c4db28f50
Nick Rose <nickfbp@gmail.com>
2020-11-10 17:23:46 -0700

    ES6 class properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,4 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
   ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,17 +5,35 @@ import Action from './Action';
 import Options from './Options';
 
 class IndecisionApp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-      this.handlePick = this.handlePick.bind(this);
-      this.handleAddOption = this.handleAddOption.bind(this);
-      this.handleDeleteOption = this.handleDeleteOption.bind(this);
-      this.state = {
-        options: []
-      };
+    state = {
+      options: []
+    };
+  
+    handleDeleteOptions = () => {
+      this.setState(() => ({ options: [] }));
     }
   
+    handleDeleteOption = (optionToRemove) => {
+      this.setState((prevState) => ({
+        options: prevState.options.filter((option) => optionToRemove !== option)
+      }))
+    }
+    handlePick = () => {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      alert(option);
+    }
+  
+    handleAddOption = (option) => {
+      if(!option) {
+        return 'Enter valid value to add item'
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exists'
+      }
+  
+      this.setState((prevState) => ({options: prevState.options.concat(option)}));
+    }
+
     componentDidMount() {
       try {
         const json = localStorage.getItem('options');
@@ -38,31 +56,6 @@ class IndecisionApp extends React.Component {
   
     componentWillUnmount() {
       console.log('componentWillUnmount!');
-    }
-  
-    handleDeleteOptions() {
-      this.setState(() => ({ options: [] }));
-    }
-  
-    handleDeleteOption(optionToRemove) {
-      this.setState((prevState) => ({
-        options: prevState.options.filter((option) => optionToRemove !== option)
-      }))
-    }
-    handlePick() {
-      const randomNum = Math.floor(Math.random() * this.state.options.length);
-      const option = this.state.options[randomNum];
-      alert(option);
-    }
-  
-    handleAddOption(option) {
-      if(!option) {
-        return 'Enter valid value to add item'
-      } else if (this.state.options.indexOf(option) > -1) {
-        return 'This option already exists'
-      }
-  
-      this.setState((prevState) => ({options: prevState.options.concat(option)}));
     }
   
     render() {
