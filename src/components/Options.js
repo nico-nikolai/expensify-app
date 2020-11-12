74e33b4c3c87c5c31e15dfb8649e85eb65a129bf
Nick Rose <nickfbp@gmail.com>
2020-11-12 15:40:14 -0700

    Styling Option Item

--- src/components/Options.js
+++ src/components/Options.js
@@ -13,10 +13,11 @@ import Option from './Option';
           </button>
         </div>
         {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
-        {props.options.map((option) => (
+        {props.options.map((option, index) => (
           <Option 
           key={option} 
           optionText={option}
+          count={index + 1}
           handleDeleteOption={props.handleDeleteOption}
           />
         ))}
