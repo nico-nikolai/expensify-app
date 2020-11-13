000f243e4c84eb2b81853b3987505daed116626c
Nick Rose <nickfbp@gmail.com>
2020-11-13 11:25:50 -0700

    Configuring Expensify App Layout

--- r-09-11-favicon/r-09-11-favicon/r-09-11-favicon/src/app.js
+++ r-09-11-favicon/r-09-11-favicon/r-09-11-favicon/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
