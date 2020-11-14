6eb2af1d4f513fbb65f489a38b9c3963b1522378
Nick Rose <nickfbp@gmail.com>
2020-11-14 16:30:40 -0700

    Combining Reducers

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,7 +19,11 @@ const resetCount = () => ({
     type: 'RESET'
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions - maintain function level scope(not global).
+// 2. Never change state or action.
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -40,7 +44,9 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
+}
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState())
