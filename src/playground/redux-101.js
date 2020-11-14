b6069d69943160fa34564070d69b96e6f5f1df75
Nick Rose <nickfbp@gmail.com>
2020-11-14 13:56:04 -0700

    Subscribing to Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,13 +3,19 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
+        case 'SET':
+            return {
+                count: action.count
+            }
         case 'RESET':
             return {
                 count: 0
@@ -19,13 +25,16 @@ const store = createStore((state = { count: 0 }, action) => {
     }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+});
 
 // Actions - an object that gets sent to the store
 
 // I'd like to increment the count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 
 store.dispatch({
@@ -42,8 +51,12 @@ store.dispatch({
     type: 'DECREMENT'
 });
 
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 
-
-
-
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
