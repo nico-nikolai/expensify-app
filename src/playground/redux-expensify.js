62edbcffe33ecee9c1aac2919f9a154692a5037f
Nick Rose <nickfbp@gmail.com>
2020-11-16 10:17:10 -0700

    Spreading Objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -26,7 +26,18 @@ const removeExpense = ({ id } = {}) => ({
 })
 
 // EDIT_EXPENSE
-// SET_TEST_FILTER
+const editExpense = (id, updates) => ({
+    type:'EDIT_EXPENSE',
+    id,
+    updates
+})
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
+
 // SORT_BY_DATE
 // SORE_BY_AMOUNT
 // SET_START_DATE
@@ -45,6 +56,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ]
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            })
         default:
             return state;
     }
@@ -61,6 +83,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
         switch (action.type) {
+            case 'SET_TEXT_FILTER':
+                return {
+                    ...state,
+                    text: action.text
+                }
             default: 
                 return state;
     }
@@ -80,9 +107,15 @@ store.subscribe(() => {
 })
 
 const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 3200 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
+
 
 const demoState = {
     expenses: [{
@@ -99,4 +132,3 @@ const demoState = {
         endDate: undefined
     }
 };
-
