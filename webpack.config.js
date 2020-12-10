96a982222d2033f6f846849c7b6bdf6325ee5310
Nick Rose <nickfbp@gmail.com>
2020-12-10 15:00:51 -0700

    Build prod webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    console.log('env', env)
+    return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -20,10 +24,11 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
     }
-};
+}
+}
 
