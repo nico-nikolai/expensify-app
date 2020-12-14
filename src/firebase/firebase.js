6e0b7151623e05fb2ab7d73cfd247b1e4232c970
Nick Rose <nickfbp@gmail.com>
2020-12-14 13:21:07 -0700

    Login/Logout buttons with testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,8 +14,9 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   const database = firebase.database();
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  export { firebase, database as default };
+  export { firebase, googleAuthProvider, database as default };
 
 //   // child_removed
 //   database.ref('expenses').on('child_removed', (snapshot) => {
